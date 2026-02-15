7c9cd53316e928399abb07d969f3fad5cc164ade
Michael Herrmann <michael@herrmann.io>
2026-02-15 12:54:36 +0100

    Add allowed_hosts

--- myproj/settings.py
+++ myproj/settings.py
@@ -120,3 +120,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(" ")
